d3c2e8c5dd463eaa72a684caf478339b81465271
Adil Aslam <aaliyanaslam22@gmail.com>
2025-02-19 14:28:20 +0500

    add navigation + bs

--- app.js
+++ app.js
@@ -10,9 +10,16 @@ fetch(`https://api.github.com/users/${input.value}`)
 .then((response => {
 
     result.innerHTML +=    `
-    <h1>${response.login} </h1>
- <img src="${response.avatar_url}" alt="">
-    
+
+    <div class="card" style="width: 18rem;">
+  <div class="card-body">
+       <h1>${response.login} </h1>
+ <img src="${response.avatar_url}" width= "100" alt="">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
     
     
     `
