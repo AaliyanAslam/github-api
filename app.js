48a55112548fbede09846b4bd10956c615fa4f08
Adil Aslam <aaliyanaslam22@gmail.com>
2025-02-20 16:07:37 +0500

    more user details and ui added

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-const form = document.getElementById('form');
-const input = document.getElementById('input');
+const form = document.getElementById("form");
+const input = document.getElementById("input");
 const result = document.querySelector(".result");
 
-
-form.addEventListener('submit', (event) =>{
-event.preventDefault();
-fetch(`https://api.github.com/users/${input.value}`)
-.then((response => response.json()))
-.then((response => {
-
-    result.innerHTML +=    `
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  fetch(`https://api.github.com/users/${input.value}`)
+    .then((response) => response.json())
+    .then((response) => {
+      result.innerHTML = `
 
     <div class="card col-md-6 col-lg-6 col-sm-12"  style="height: 28rem; width: 50rem;">
   <div class="card-body d-flex justify-content-between">
@@ -24,11 +22,12 @@ fetch(`https://api.github.com/users/${input.value}`)
   </div>
   <div class="more"> 
    
-  <div>Followers: ${response.followers}</div>
-  <div>Following: ${response.following}</div>
-  <div>Followers: ${response.followers}</div>
-  <div>Followers: ${response.followers}</div>
-  <div>Followers: ${response.followers}</div>
+  <div class="user-more">Bio: ${response.bio}</div>
+  <div class="user-more">Company: ${response.company}</div>
+  <div class="user-more">Location: ${response.location}</div>
+  <div class="user-more">Followers: ${response.followers}</div>
+  <div class="user-more">Following: ${response.following}</div>
+  <div class="user-more">Public Repositories : ${response.public_repos}</div>
 
   
  
@@ -37,19 +36,8 @@ fetch(`https://api.github.com/users/${input.value}`)
 </div>
     
     
-    `
-    console.log(response);
-    
-  
-    
-  
-    
-}))
-
-
-})
-
-
-
-
-
+    `;
+      input.value = "";
+      console.log(response);
+    });
+});
